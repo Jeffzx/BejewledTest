974f00e1989f9023dab3241543b2a6bc7fca69b4
zx <zx@zxs-MacBook-Pro.local>
2015-04-28 11:20:47 +0800

    Initial Commit

---
+++ Bejewled/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Bejewled
+//
+//  Created by zx on 28/04/2015.
+//  Copyright (c) 2015 zx. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
